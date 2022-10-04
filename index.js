637a2b4318c9ef7f7a57245cd98bb4177f6d4866
Yuriy Pliskovsky <zzaafamania@gmail.com>
2022-10-04 18:15:07 +0300

    version 1.0

---
+++ index.js
@@ -0,0 +1,37 @@
+const filterByAge = () => {
+  // your code
+};
+
+const filterByScore = () => {
+  // your code
+};
+
+const filterByServiceRecord = () => {
+  // your code
+};
+
+const sortByScore = () => {
+  // your code
+};
+
+const getMeanAge = () => {
+  // your code
+};
+
+const getMeanAgeReduce = () => {
+  // your code
+};
+
+const getNameOnly = () => {
+  // your code
+};
+
+module.exports = {
+  filterByAge,
+  filterByScore,
+  filterByServiceRecord,
+  sortByScore,
+  getMeanAge,
+  getMeanAgeReduce,
+  getNameOnly,
+};
